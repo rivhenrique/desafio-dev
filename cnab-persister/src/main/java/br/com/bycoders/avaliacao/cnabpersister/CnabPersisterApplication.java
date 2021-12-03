4d2eec8cac78969c11652080eddfb703bbb3debb
Henrique Rivero <rivhenrique@gmail.com>
2021-12-03 19:53:08 -0300

    [ADDED] -- Commit inicial com início do código

---
+++ cnab-persister/src/main/java/br/com/bycoders/avaliacao/cnabpersister/CnabPersisterApplication.java
@@ -0,0 +1,13 @@
+package br.com.bycoders.avaliacao.cnabpersister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnabPersisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnabPersisterApplication.class, args);
+	}
+
+}
---
+++ cnab-persister/src/test/java/br/com/bycoders/avaliacao/cnabpersister/CnabPersisterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bycoders.avaliacao.cnabpersister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnabPersisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
