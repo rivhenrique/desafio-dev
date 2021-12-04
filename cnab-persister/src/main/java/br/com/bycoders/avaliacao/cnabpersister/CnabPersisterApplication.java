48afdfab2ffd7c4aebc46756ee3ce6a802b2c0d1
Henrique Rivero <rivhenrique@gmail.com>
2021-12-04 17:42:18 -0300

    [ADDED] - Contexto da aplicação

--- cnab-persister/src/main/java/br/com/bycoders/avaliacao/cnabpersister/CnabPersisterApplication.java
+++ cnab-persister/src/main/java/br/com/bycoders/avaliacao/cnabpersister/CnabPersisterApplication.java
@@ -3,7 +3,7 @@ package br.com.bycoders.avaliacao.cnabpersister;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"br.com.bycoders.avaliacao.cnabpersister"})
 public class CnabPersisterApplication {
 
 	public static void main(String[] args) {
