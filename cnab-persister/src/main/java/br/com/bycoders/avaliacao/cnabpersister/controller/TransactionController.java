381da40f4d140c46d58128afc390e5c6a47d25b2
Henrique Rivero <rivhenrique@gmail.com>
2021-12-05 20:07:26 -0300

    [ADDED] - Página de upload de arquivo

--- cnab-persister/src/main/java/br/com/bycoders/avaliacao/cnabpersister/controller/TransactionController.java
+++ cnab-persister/src/main/java/br/com/bycoders/avaliacao/cnabpersister/controller/TransactionController.java
@@ -3,6 +3,7 @@ package br.com.bycoders.avaliacao.cnabpersister.controller;
 import br.com.bycoders.avaliacao.cnabpersister.dto.StoresTransactionsResponseDTO;
 import br.com.bycoders.avaliacao.cnabpersister.service.CnabFileService;
 import br.com.bycoders.avaliacao.cnabpersister.service.TransactionService;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,9 +26,10 @@ public class TransactionController {
     }
 
     @PostMapping(value = "/cnab-file/upload")
-    public String cnabFileUpload(@RequestParam("file") MultipartFile file) {
+    public String cnabFileUpload(@RequestParam("file") MultipartFile file, Model model) {
         cnabFileService.processFile(file);
-        return "iei, foi";
+        return "Upload e processamento do arquivo realizado com sucesso! Para ver a lista completa clique no link: \n" +
+                "<a href=\"http://localhost:8080/cnab-persister/transaction/all\">Listar Transações</a>\n";
     }
 
 }
